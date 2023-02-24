3754f9b1e89fb58eb90061f24843f71a5f9d6481
lavakumar <lavakuriti@gmail.com>
2023-02-24 09:21:32 +0530

    se

---
+++ sern.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send(`<!DOCTYPE html>
+    <html>
+        <head>
+    
+        </head>
+        <body>
+            <h1>Hi</h1>
+        </body>
+    </html>`)
+})
+app.listen(8000)
