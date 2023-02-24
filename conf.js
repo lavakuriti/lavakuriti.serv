8dad311784a00ddbdb26bee4336322341cf48797
lavakumar <lavakuriti@gmail.com>
2023-02-24 13:50:59 +0530

    w2

--- conf.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const path=require('path');
-const app=express();
-module.exports=(app)=>{
-app.use(express.json({
-    type:"*/*"
-}));
-app.set('views',path.join(__dirname,'views'));
-}
