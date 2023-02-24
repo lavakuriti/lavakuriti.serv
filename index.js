f330e69bbff188bdcd67f219d2ea941f43d8455a
lavakumar <lavakuriti@gmail.com>
2023-02-24 15:04:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
 app.get('/',(req,res)=>{
-    res.render('ee')
+    res.send(`<!DOCTYPE html>
+    <html>
+        <head>
+    
+        </head>
+        <body>
+            <h1>kuyo8i8o</h1>
+        </body>
+    </html>`)
 })
 app.listen(8000);
