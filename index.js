bedb17b9150684f1d38134ac1b51f6ad42df5d94
lavakumar <lavakuriti@gmail.com>
2023-02-24 14:02:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const express=require('express');
-const config=require('./conf.js');
-const rout=require('./router.js');
-const app=express();
-app.set('view engine','ejs');
-config(app);
-rout(app);
+const express=require('express')
+const app=express()
+
 app.listen(8000);
