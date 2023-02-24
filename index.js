c96e95fdc5d360b826129be21f0c9e443338f7ee
lavakumar <lavakuriti@gmail.com>
2023-02-24 14:08:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express=require('express')
 const app=express()
 app.get('/',(req,res)=>{
-    res.send("OK")
+    res.send(`<!DOCTYPE html>
+    <html>
+        <head>
+    
+        </head>
+        <body>
+            <h1>kwq</h1>
+        </body>
+    </html>`)
 })
 app.listen(8000);
